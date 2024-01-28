dc5affd4163f6e14cb18c3e409d4a38ecaf63082
CyN1ckal <warrior276276276@gmail.com>
2024-01-28 12:38:56 -0800

    1

--- BlackOps2-Kiero/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct config
-{
-public:
-	uintptr_t BaseAddress;
-	DWORD PID;
-};
-config Config;
--- BlackOps2-Kiero/globalVariables.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-HMODULE g_hModule;
-HANDLE hConsole;
-FILE* f;
-time_t CurrentTime;
